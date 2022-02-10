c9db0e9c908a15e8571576b5ee8488a6cee559e1
Kishan Takoordyal <kishan@kinesis.games>
2022-02-10 12:50:28 +0400

    update API_URL

--- src/wrappers/LocalContext.jsx
+++ src/wrappers/LocalContext.jsx
@@ -3,8 +3,8 @@ import React, { useState } from 'react';
 const LocalContext = React.createContext();
 
 function LocalContextProvider({ children }) {
-  const API_URL = 'http://localhost:8000';
-  // const API_URL = 'https://api.kinesis.world';
+  // const API_URL = 'http://localhost:8000';
+  const API_URL = 'https://api.kinesis.world';
 
   const [posts, setPosts] = useState([]);
 
